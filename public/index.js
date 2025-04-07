41bb789788a3cf4d700e64191aa1bc57b989e93f
Ernest Chidakwa <149294108+E40sr@users.noreply.github.com>
2025-04-07 18:06:10 +0200

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,67 +1,52 @@
-const socket = io('/');
+const socket = io();
 const peer = new Peer();
-let myVideoStream;
-let myId;
-var videoGrid = document.getElementById('videoDiv')
-var myvideo = document.createElement('video');
-myvideo.muted = true;
-const peerConnections = {}
-navigator.mediaDevices.getUserMedia({
-  video:true,
-  audio:true
-}).then((stream)=>{
-  myVideoStream = stream;
-  addVideo(myvideo , stream);
-  peer.on('call' , call=>{
-    call.answer(stream);
-      const vid = document.createElement('video');
-    call.on('stream' , userStream=>{
-      addVideo(vid , userStream);
-    })
-    call.on('error' , (err)=>{
-      alert(err)
-    })
-    call.on("close", () => {
-        console.log(vid);
-        vid.remove();
-    })
-    peerConnections[call.peer] = call;
-  })
-}).catch(err=>{
-    alert(err.message)
-})
-peer.on('open' , (id)=>{
-  myId = id;
-  socket.emit("newUser" , id , roomID);
-})
-peer.on('error' , (err)=>{
-  alert(err.type);
+let localStream;
+let currentCall;
+const myVideo = document.getElementById('myVideo');
+const remoteVideo = document.getElementById('remoteVideo');
+const nextBtn = document.getElementById('nextBtn');
+
+navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+  .then(stream => {
+    localStream = stream;
+    myVideo.srcObject = stream;
+  });
+
+peer.on('open', id => {
+  socket.emit('register', id);
 });
-socket.on('userJoined' , id=>{
-  console.log("new user joined")
-  const call  = peer.call(id , myVideoStream);
-  const vid = document.createElement('video');
-  call.on('error' , (err)=>{
-    alert(err);
-  })
-  call.on('stream' , userStream=>{
-    addVideo(vid , userStream);
-  })
-  call.on('close' , ()=>{
-    vid.remove();
-    console.log("user disconect")
-  })
-  peerConnections[id] = call;
-})
-socket.on('userDisconnect' , id=>{
-  if(peerConnections[id]){
-    peerConnections[id].close();
-  }
-})
-function addVideo(video , stream){
-  video.srcObject = stream;
-  video.addEventListener('loadedmetadata', () => {
-    video.play()
-  })
-  videoGrid.append(video);
-}
+
+socket.on('partner', partnerId => {
+  const call = peer.call(partnerId, localStream);
+  setupCall(call);
+});
+
+peer.on('call', call => {
+  call.answer(localStream);
+  setupCall(call);
+});
+
+socket.on('partner-disconnected', () => {
+  if (currentCall) currentCall.close();
+  remoteVideo.srcObject = null;
+});
+
+socket.on('waiting', () => {
+  remoteVideo.srcObject = null;
+  if (currentCall) currentCall.close();
+});
+
+nextBtn.onclick = () => {
+  socket.emit('next');
+};
+
+function setupCall(call) {
+  if (currentCall) currentCall.close();
+  currentCall = call;
+  call.on('stream', stream => {
+    remoteVideo.srcObject = stream;
+  });
+  call.on('close', () => {
+    remoteVideo.srcObject = null;
+  });
+                                        }
